f25b52d0389d40e99c1cdb22d87d041b90f26895
Ezequiel <ezequiel.bitencourt197@gmail.com>
2020-09-09 10:39:42 -0300

    add Ex 5 e 6

---
+++ Laboratorio1/Ex5to6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio1
+{
+    class Ex5to6
+    {
+        public void ConversorSys()
+        {
+            string stringInteiro = "123456789";
+            int valorStringInteiro = Convert.ToInt32(stringInteiro);
+
+            Console.WriteLine(valorStringInteiro);
+
+            Int64 valorInt64 = 123456789;
+            int valorInt = Convert.ToInt32(valorInt64);
+
+            Console.WriteLine(valorInt);
+
+            string stringInteiroGrande = "999999999999999999999999999999999999999999999";
+            //int valorStringInteiroGrande = Convert.ToInt32(stringInteiroGrande); ERRO!!
+
+        }
+
+        public void ConversorTryparse()
+        {
+            string stringInteiro = "123456789";
+            int valorStringInteiro;
+            bool conversao1 = Int32.TryParse(stringInteiro, out valorStringInteiro);
+
+            Console.WriteLine("Conversão efetuada:" + conversao1 + " Valor: " + valorStringInteiro);
+
+            string stringInteiroGrande = "999999999999999999999999999999999999999999999";
+            int valorStringInteiroGrande;
+            bool conversao2 = Int32.TryParse(stringInteiroGrande, out valorStringInteiroGrande);
+
+            Console.WriteLine("Conversão efetuada:" + conversao2 + " Valor: " +
+            valorStringInteiroGrande);
+
+            string stringLetras = "abc";
+            double valorStringLetras;
+            bool conversao3 = Double.TryParse(stringLetras, out valorStringLetras);
+
+            Console.WriteLine("Conversão efetuada:" + conversao3 + " Valor: " + valorStringLetras);
+        }
+    }
+}
--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -8,10 +8,13 @@ namespace Laboratorio1
         {
             Ex1to3 ex1To3 = new Ex1to3();
             Ex4 ex4 = new Ex4();
+            Ex5to6 ex5 = new Ex5to6();
 
             ex1To3.Ex();
             ex4.StringDia();
             ex4.Conversor();
+            ex5.ConversorSys();
+            ex5.ConversorTryparse();
 
 
 
