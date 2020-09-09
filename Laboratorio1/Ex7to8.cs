be30ca3a54008ffa48b95dc6fd439cfc51a4a23f
Ezequiel <ezequiel.bitencourt197@gmail.com>
2020-09-09 10:50:50 -0300

    add Ex 7 e 8

---
+++ Laboratorio1/Ex7to8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio1
+{
+    class Ex7to8
+    {
+        public void Arredondamento()
+        {
+            double valorFracionado = 4.7;
+            int valorInteiro1 = (int)valorFracionado;
+            int valorInteiro2 = Convert.ToInt32(valorFracionado);
+
+            Console.WriteLine("Conversao explicita = " + valorInteiro1);
+            Console.WriteLine("Conversao metodo Convert = " + valorInteiro2);
+        }
+
+        public void Exibicao()
+        {
+            int x = 10;
+            double y = 3.4;
+            Console.WriteLine("{0} {1}", x, y);
+        }
+    }
+}
--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -9,12 +9,15 @@ namespace Laboratorio1
             Ex1to3 ex1To3 = new Ex1to3();
             Ex4 ex4 = new Ex4();
             Ex5to6 ex5 = new Ex5to6();
+            Ex7to8 ex7To8 = new Ex7to8();
 
             ex1To3.Ex();
             ex4.StringDia();
             ex4.Conversor();
             ex5.ConversorSys();
             ex5.ConversorTryparse();
+            ex7To8.Arredondamento();
+            ex7To8.Exibicao();
 
 
 
