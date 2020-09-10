88cb26ae4e29ceb9f0a80bc56532040f9062ca44
Ezequiel Bitencourt <Ezequiel_Bittencourt@Dellteam.com>
2020-09-10 15:42:54 -0300

    Exemplos lab2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Laboratório 2 – Arrays
+Este laboratório introduz o uso de arrays.
+*/
+
+using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExArrays exArrays = new ExArrays();
+           
+            Console.WriteLine("Ex 1:");
+            exArrays.Ex1();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ex 2:");
+            exArrays.Ex2();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Ex 3:");
+            exArrays.Ex3();
+        }
+    }
+}
