8e933af39b04c3da68bb111c8ad761327f1fadd1
Ezequiel Bitencourt <Ezequiel_Bittencourt@Dellteam.com>
2020-09-11 14:47:51 -0300

    update Ex4

--- Laboratorio3/Laboratorio3/ContaCorrente.cs
+++ Laboratorio3/Laboratorio3/ContaCorrente.cs
@@ -8,9 +8,24 @@ namespace Laboratorio3
     {
         private decimal saldo;
 
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+
+        public ContaCorrente(decimal val)
+        {
+            saldo = val;
+        }
+
         public void Depositar(decimal val)
         {
             saldo = saldo + val;
+        }
+
+        public void Sacar(decimal val)
+        {
+            saldo = saldo - val;
         }
     }
 }
--- Laboratorio3/Laboratorio3/Program.cs
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -6,8 +6,20 @@ namespace Laboratorio3
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaCorrente = new ContaCorrente();
+            /*Exemplos
+            ContaCorrente minhaConta = new ContaCorrente(340);
 
+            Console.WriteLine(minhaConta.Saldo);
+            minhaConta.Depositar(100);
+            Console.WriteLine(minhaConta.Saldo);
+            minhaConta.Sacar(50);
+            Console.WriteLine(minhaConta.Saldo);
+            */
+
+            //Exercicio 4
+            ContaCorrenteEx4 minhaContaEx4 = new ContaCorrenteEx4(100,"Ezequiel");
+
+            minhaContaEx4.DadosCliente();
 
         }
     }
