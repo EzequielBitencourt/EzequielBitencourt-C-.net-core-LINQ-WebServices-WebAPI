da761772f726bc312b96089fc96090b4c56ea1a1
Ezequiel Bitencourt <Ezequiel_Bittencourt@Dellteam.com>
2020-09-11 14:19:25 -0300

    add lab3

---
+++ Laboratorio3/Laboratorio3/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio3
+{
+    public class ContaCorrente
+    {
+        private decimal saldo;
+
+        public void Depositar(decimal val)
+        {
+            saldo = saldo + val;
+        }
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+
+        }
+    }
+}
